58e54cbcdf2ce430735f0b8626df2154f2124106
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2021-05-21 16:06:39 +0700

    first commit

---
+++ ifElse_script.js
@@ -0,0 +1,43 @@
+// if 1
+// bai 4: Nhap 3 so nguyen, tim gia tri lon nhat.
+let max = 0
+
+function findMax(num1, num2, num3){
+    if (num1 > num2){
+        if (num1 > num3){
+            max = num1;
+        }
+        else{
+            max = num3;
+        }
+    }
+    else {
+        if (num2 > num3){
+            max = num2;
+        }
+        else{
+            max = num3;
+        }
+        }
+    return max;
+}
+
+// let myArr = []
+// for (let i=0; i<3; i++){
+//     myArr[i] = parseInt(prompt("Enter an integer: "));
+// }
+
+function getMax() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+    let c = document.getElementById("num3").value;
+    document.getElementById("result").innerHTML = "<h4>Result: " + findMax() + "</h4>";
+}
+
+
+// if 2
+// bai 6
+// bai 7
+// bai 12
+
+
---
+++ loop_script.js
@@ -0,0 +1,10 @@
+// loop1
+// bai 2
+// bai 4
+// bai 5
+// bai 7
+
+// loop2
+// bai 2
+// bai 3 -> nested loops, 1 cai cho row, 1 cai cho column
+//https://github.com/minhle0105/PracticeProject/tree/main/advancedForLoopPractice
